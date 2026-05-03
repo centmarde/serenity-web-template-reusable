7ea6d3c71d295642d9efd30a4c3e0c69304ad6d6
BritneySpears <159101935+centmarde@users.noreply.github.com>
2026-05-03 13:26:30 +0800

    Refactor code structure for improved readability and maintainability

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'chubabi',
+  appName: 'chubabi',
+  webDir: 'build'
+};
+
+export default config;
